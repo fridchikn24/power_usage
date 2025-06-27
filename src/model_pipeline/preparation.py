ad0a8cd8a22916aa7f08c82da2821a022e6a119b
Sameeullah Babar <fridchikn2@gmail.com>
2025-06-26 19:33:11 -0500

    finished data prepper class

--- src/model_pipeline/preparation.py
+++ src/model_pipeline/preparation.py
@@ -21,18 +21,53 @@ class DataPrepper:
 
         self.df = df
 
-    def decompose_dates(self):
+    def decompose_dates(self,df):
 
         logger.info("decomposing datetime")
-        self.df['date'] = pd.to_datetime(self.df['date'])
+        df['date'] = pd.to_datetime(df['date'])
 
-        self.df['Year'] = self.df['date'].dt.year
-        self.df['Month'] = self.df['date'].dt.month
-        self.df['Day_of_Week']=self.df['date'].dt.dayofweek
-        self.df['Is_Weekend'] = self.df['Day_of_Week'] >= 5
+        df['Year'] = df['date'].dt.year
+        df['Month'] = df['date'].dt.month
+        df['Day_of_Week']=df['date'].dt.dayofweek
+        df['Is_Weekend'] = df['Day_of_Week'] >= 5
     
-    def identify_holidays(self):
+    def identify_holidays(self, df):
+       
         logger.info("Checking for holidays")
         us_holidays = holidays.US(years=[2022,2023,2024])
         # Check if each date is a holiday
-        self.df['Is_Holiday'] = self.df['date'].isin(us_holidays)
+        df['Is_Holiday'] = df['date'].isin(us_holidays)
+
+    def create_lag(self,df):
+
+        """
+        function to create lagged y variables based on the "active power" variable
+        """
+
+        logger.info('creating lag variables')
+        for lag in [1, 2, 5,7]:
+            df[f'lag_{lag}'] = df['active_power'].shift(lag)
+
+    def create_moving_averages(self,df):
+        """
+        create moving averages and standard deviation 
+        for the active power variable
+        """
+        for window in [3, 7]:
+            df[f'roll_mean_{window}'] = df['active_power'].shift(1).rolling(window=window).mean()
+            df[f'roll_std_{window}'] = df['active_power'].shift(1).rolling(window=window).std()
+
+
+    def transform(self):
+        """
+        Runs all transformation steps in sequence and returns the final engineered DataFrame.
+        """
+        df = self.df.copy()
+
+        self.decompose_dates(df)
+        self.identify_holidays(df)
+        self.create_lag(df)
+        self.create_moving_averages(df)
+
+        df = df.dropna().reset_index(drop=True)  # Remove NaNs from lag/rolling ops
+        return df
